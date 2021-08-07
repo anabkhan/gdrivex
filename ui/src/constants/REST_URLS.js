b349c47f1fde2b576721d912791c5eff1ca44594
anabkhan <akanabkhan@gmail.com>
2021-08-07 16:13:05 +0530

    refactoring

---
+++ ui/src/constants/REST_URLS.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://localhost:3001/"
+export const ADD_DRIVE = "addDrive"
+export const SUBMIT_AUTH = "submitAuthCode"
+export const LIST_DRIVES = "listDrives"
---
+++ ui/src/services/RestService.js
@@ -0,0 +1,17 @@
+import { BASE_URL } from "../constants/REST_URLS"
+
+export const get = (url) => {
+    return fetch(BASE_URL + url).then((response) => response.json());
+}
+
+export const post = (url, body) => {
+    return fetch(BASE_URL + url, {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then((response) => response.json());
+}
